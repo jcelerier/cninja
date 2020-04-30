7157c9f805e7606a5d82ad322b907a06a2dae6c2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-04-30 23:58:10 +0200

    Fix gcc lto

--- src/builtins/lto.cmake
+++ src/builtins/lto.cmake
@@ -2,12 +2,15 @@
 cninja_optional(compiler)
 set(CNINJA_LTO_KIND "%lto%")
 
-string(APPEND CMAKE_C_FLAGS_INIT " -flto=%lto%")
-string(APPEND CMAKE_CXX_FLAGS_INIT " -flto=%lto%")
-
 if("clang" STREQUAL "${CNINJA_COMPILER}")
+  string(APPEND CMAKE_C_FLAGS_INIT " -flto=%lto%")
+  string(APPEND CMAKE_CXX_FLAGS_INIT " -flto=%lto%")
+
   string(APPEND CMAKE_C_FLAGS_INIT " -fwhole-program-vtables")
   string(APPEND CMAKE_CXX_FLAGS_INIT " -fwhole-program-vtables")
+else()
+  string(APPEND CMAKE_C_FLAGS_INIT " -flto")
+  string(APPEND CMAKE_CXX_FLAGS_INIT " -flto")
 endif()
 # Will only be available in clang-11:
 # -fvirtual-function-elimination
