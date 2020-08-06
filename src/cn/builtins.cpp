c1f7dfe87552234191601145ae34b55788094126
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-08-06 16:35:11 +0200

    Remove dead code

--- src/cn/builtins.cpp
+++ src/cn/builtins.cpp
@@ -6,20 +6,6 @@
 #include "builtin_setup.hpp"
 namespace cn
 {
-namespace
-{
-#if defined(_WIN32)
-std::string to_cmake_path(std::string input)
-{
-  for(char& c : input)
-    if(c == '\\')
-      c = '/';
-  return input;
-}
-#else
-#define to_cmake_path(str) str
-#endif
-}
 const builtin_map& builtins()
 {
   static const builtin_map map = [] {
