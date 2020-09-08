cae5813930f92d2b496e447b83d62976ffcdb32f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-08 12:43:40 +0200

    Unset _INIT flags on startup to prevent flag duplication

--- src/builtins/start.cmake
+++ src/builtins/start.cmake
@@ -4,6 +4,15 @@ else()
   cmake_policy(VERSION 3.9)
 endif()
 
+foreach(LANG C CXX OBJC OBJCXX CUDA FORTRAN ASM)
+  unset(CMAKE_${LANG}_FLAGS_INIT)
+endforeach()
+
+unset(CMAKE_EXE_LINKER_FLAGS_INIT)
+unset(CMAKE_SHARED_LINKER_FLAGS_INIT)
+unset(CMAKE_MODULE_LINKER_FLAGS_INIT)
+
+
 macro(add_linker_flags)
   string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT ${ARGV})
   string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT ${ARGV})
