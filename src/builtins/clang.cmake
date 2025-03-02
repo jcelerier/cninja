43119542da89bfc88aaad3172df1ebb74ea400a5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-02 17:05:22 +0100

    clang: support newer versions

--- src/builtins/clang.cmake
+++ src/builtins/clang.cmake
@@ -3,14 +3,12 @@ cninja_require(pre)
 set(CNINJA_COMPILER clang)
 
 find_program(CMAKE_C_COMPILER NAMES
+  clang-25 clang-24 clang-23 clang-22 clang-21 
   clang-20 clang-19 clang-18 clang-17 clang-16
-  clang-15 clang-14 clang-13 clang-12 clang-11
-  clang-10 clang-9  clang-8  clang-7  clang-6
   clang
 )
 find_program(CMAKE_CXX_COMPILER NAMES
+  clang++-25 clang++-24 clang++-23 clang++-22 clang++-21 
   clang++-20 clang++-19 clang++-18 clang++-17 clang++-16
-  clang++-15 clang++-14 clang++-13 clang++-12 clang++-11
-  clang++-10 clang++-9  clang++-8  clang++-7  clang++-6
   clang++
 )
